9054f2d2fc8807292396618b563a1e78abf4becd
Tejas Patel <pateltejas.2005@gmail.com>
2026-03-27 17:01:01 +0530

    updated api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello from the CI/CD Implentation App!');
+    res.send('Hello from the Updated CI/CD Implentation App!');
 });
 
 app.listen(PORT, () => {
